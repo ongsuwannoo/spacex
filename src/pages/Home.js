2291e81757645c508e5e9e2a57b26d07e5025f2d
aprilsrp <43024156+aprilsrp@users.noreply.github.com>
2021-02-20 03:02:06 +0700

    ้update home + nav
    ยังไม่ได้แก้ขนาด font ให้ responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useInfo } from '../utils/useAPIs';
+import '../styles/Home.css'
 
 const Home = () => {
 
@@ -11,17 +12,50 @@ const Home = () => {
         ? <p>Error fetching Info: {error}</p>
         :
         <>
-          <p>ชื่อ: {info.name}</p>
-          <p>ผู้ก่อตั้ง: {info.founder}</p>
-          <p>ก่อตั้งเมื่อ: {info.founded}</p>
-          <p>มีพนักงาน: {info.employees}</p>
-          <p>ceo/cto: {info.ceo}</p>
-          <p>coo: {info.coo}</p>
-          <p>มูลค่าบริษัท: {info.valuation}</p>
-          <p>สำนักงานใหญ่: {info.headquarters.city} / {info.headquarters.state}</p>
-          <p>website: {info.links.website}</p>
-          <p>flickr: {info.links.flickr}</p>
-          <p>twitter: {info.links.twitter}</p>
+          <div className="sec1">
+            <img className="homeImg" src="https://pbs.twimg.com/profile_banners/34743251/1596331248/1500x500"></img>
+            <div className="homeInfo1">
+              <div>
+                <b className="fs-25">{info.coo}</b>
+                <p className="fs-20">COO</p>
+              </div>
+              <div style={{marginTop:"1vw"}}>
+                <b className="fs-25">{info.ceo}</b>
+                <p className="fs-20">Founder / CEO /CTO</p>
+              </div>
+            </div>
+            <div className="homeInfo2">
+              <div> 
+                <b className="fs-25">{info.employees}</b>
+                <p className="fs-20">Employees</p>
+              </div>
+              <div style={{marginTop:"1vw"}}>
+                <b className="fs-25">{info.valuation}</b>
+                <p className="fs-20">Valuation</p>
+              </div>
+              <div style={{marginTop:"1vw"}}>
+                <b className="fs-25">{info.headquarters.city} ,{info.headquarters.state}</b>
+                <p className="fs-20">Headquarters</p> 
+              </div>           
+            </div>
+          </div>
+          <div className="sec2">
+            <div>
+              <img className="elonImg" src={'/ElonMusk.jpg'}></img>
+            </div>
+            <div className="homeInfo3">
+              <p className="fs-30">{info.summary}</p>
+              <br></br>
+              <img className="contact" src={'/flickr.png'}></img>
+              <img className="contact" src={'/web.png'}></img>
+              <img className="contact" src={'/twitter.png'}></img>
+            </div>            
+            
+            {/* <p>website: {info.links.website}</p>
+            <p>flickr: {info.links.flickr}</p>
+            <p>twitter: {info.links.twitter}</p> */}
+          </div>
+
         </>
       }
     </>
