4ec51d0b521a8c0e66cf6d04cd3a9b1dc14672c3
Jakkrapat Suwanno <ongsuwannoo@gmail.com>
2021-02-20 20:14:02 +0700

    Edit rockets page and detail

--- src/pages/RocketDetail.js
+++ src/pages/RocketDetail.js
@@ -12,11 +12,79 @@ export default (props) => {
     <div>
       {error !== null || details === null
         ? <p>Error fetching Info: {error}</p>
+        : (
+          <div className="box rocket-detail-box">
+            <div className="box-bg">
+              <div style={{ position: "relative" }}>
+                <div className="home-bg-content-1">
+                  <p>
+                    <label className="f-5">
+                      No data
+                    </label>
+                    <br />
+                    COO
+                    </p>
+                  <p></p>
+                  <p>
+                    <label className="f-5">
+                      No data
+                    </label>
+                    <br />
+                    Founder / CEO / CTO
+                  </p>
+                </div>
+                <div className="home-bg-content-2">
+                  <p>
+                    <label className="f-5">
+                      No data
+                </label>
+                    <br />
+                Employees
+              </p>
+                  <p></p>
+                  <p>
+                    <label className="f-5">
+                      No data
+                </label>
+                    <br />
+                  Valuation
+              </p>
+                  <p></p>
+                  <p>
+                    <label className="f-5">
+                      No data
+                </label>
+                    <br />
+                  Headquarters
+              </p>
+                </div>
+              </div>
+
+              <img className="home-bg rocket-detail" src={details.flickr_images[2]} />
+              <div className="home-content">
+                <div className="home-box-elon">
+                  <img className="home-elon" src='/spacex/media/Elon_Musk.jpg' />
+                </div>
+                <div className="home-box-info">
+                  <div className="home-info-detail">
+                    <p>No data</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )
+      }
+
+
+      {/* {error !== null || details === null
+        ? <p>Error fetching Info: {error}</p>
         : <Slider images={details.flickr_images} />
       }
       <pre>
         <code>Detail {JSON.stringify(details, null, 2)}</code>
-      </pre>
+      </pre> */}
     </div>
+
   )
 }
